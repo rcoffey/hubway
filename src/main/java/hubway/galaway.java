37c1c60d3b0eae10faff0bb12fa39e57321390ae
Constance <Baltera@galatea-associates.com>
2014-05-28 17:27:12 -0400

    Add querying hubway API
    for stations, but we should probably use it for rides.

--- src/main/java/hubway/galaway.java
+++ src/main/java/hubway/galaway.java
@@ -2,9 +2,15 @@ package hubway;
 
 import hubway.utility.Calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -38,8 +44,41 @@ public class galaway {
 			stationList.add(new Station(stationObj));
 		}
         Calculator.printMinMaxStations(stationList);        
+        
+        try {
+			URL stationQuery = new URL("http://hubwaydatachallenge.org/api/v1/station/?format=json&username=cbaltera&api_key=25f3498d4e7f722a0ed6f3757542669b443e21a6&name__icontains=Boston");
+			BufferedReader stationReader = new BufferedReader(new InputStreamReader(stationQuery.openStream()));
+        
+			String stationJSON = stationReader.readLine(); // hopefully it's one line
+			JSONObject bostonStations = new JSONObject(stationJSON);
+			
+			System.out.println(bostonStations.toString(2));
+        
+        } catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
 
+	/**
+	 * hubway api url/queries:
+	 * http://hubwaydatachallenge.org/api/v1/station/
+	 * ?format=json&username=cbaltera&api_key=25f3498d4e7f722a0ed6f3757542669b443e21a6
+	 * &name__icontains=Boston
+	 * 
+	 * The api doesn't have municipality available for stations. nb_docks is a in a 
+	 * separate schema from the basic station info.  nb_bikes and nb_emptyDocks are also
+	 * available.
+	 * Trip data appears to be the same, so maybe keep stations in mongo and get trips from 
+	 * the hubway api.
+	 * 
+	 * http://hubwaydatachallenge.org/api/v1/trip/
+	 * ?format=json&username=cbaltera&api_key=25f3498d4e7f722a0ed6f3757542669b443e21a6
+	 * &duration__gt=3600&start_station=33&start_date__gte=2011-08-01&end_date__lte=2011-08-31
+	 */
 }
