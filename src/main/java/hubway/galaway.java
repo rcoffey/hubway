7c761191b3b9a5f6ba0bc7fa9760fcd4d173df9c
Constance <Baltera@galatea-associates.com>
2014-05-29 10:50:25 -0400

    use calculator

--- src/main/java/hubway/galaway.java
+++ src/main/java/hubway/galaway.java
@@ -1,5 +1,7 @@
 package hubway;
 
+import hubway.utility.Calculator;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -63,7 +65,7 @@ public class galaway {
 
 		System.out.println("There are " + stationList.size() + " stations");
 
-		printMinMaxStations(stationList);
+		Calculator.printMinMaxStations(stationList);
 
 		try {
 			URL stationQuery = new URL(HubwayStationURL
@@ -85,72 +87,7 @@ public class galaway {
 		}
 	}
 
-	private static void printMinMaxStations(List<Station> stationList) {
-		Double minDist = Double.MAX_VALUE;
-		Double maxDist = Double.MIN_VALUE;
-		Station maxStationStart = null, maxStationDest = null, minStationStart = null, minStationDest = null;
 
-		for (Iterator<Station> itStart = stationList.iterator(); itStart
-				.hasNext();) {
-			Station start = (Station) itStart.next();
-
-			for (Iterator<Station> itDest = stationList.iterator(); itDest
-					.hasNext();) {
-				Station dest = (Station) itDest.next();
-
-				if (start.station.equalsIgnoreCase(dest.station))
-					continue;
-
-				Double dist = distFrom(dest.lat, dest.lng, start.lat, start.lng);
-
-				if (dist < minDist) {
-					minDist = dist;
-					minStationStart = start;
-					minStationDest = dest;
-				}
-
-				if (dist > maxDist) {
-					maxDist = dist;
-					maxStationStart = start;
-					maxStationDest = dest;
-				}
-			}
-		}
-
-		System.out
-				.println("minStationStart.station() = "
-						+ minStationStart.station + ", "
-						+ minStationStart.municipality);
-		System.out.println("minStationDest.station() = "
-				+ minStationDest.station + ", " + minStationDest.municipality);
-		System.out.println("minDist() = " + minDist);
-
-		System.out
-				.println("maxStationStart.station() = "
-						+ maxStationStart.station + ", "
-						+ maxStationStart.municipality);
-		System.out.println("maxStationDest.station() = "
-				+ maxStationDest.station + ", " + maxStationDest.municipality);
-		System.out.println("maxDist() = " + maxDist);
-	}
-
-	// stolen from the internets.
-	public static double distFrom(double lat1, double lng1, double lat2,
-			double lng2) {
-		double earthRadius = 3958.75; // units?
-		double dLat = Math.toRadians(lat2 - lat1);
-		double dLng = Math.toRadians(lng2 - lng1);
-		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-				+ Math.cos(Math.toRadians(lat1))
-				* Math.cos(Math.toRadians(lat2)) * Math.sin(dLng / 2)
-				* Math.sin(dLng / 2);
-		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		double dist = earthRadius * c;
-
-		int meterConversion = 1609;
-
-		return (double) (dist * meterConversion);
-	}
 
 	/**
 	 * hubway api url/queries: http://hubwaydatachallenge.org/api/v1/station/
