bd7b4e6a6b4f99ed89b7695562cee76921f63427
rcoffey <coffey@galatea-associates.com>
2014-06-04 17:54:52 -0400

    fix hashmap

--- src/main/java/hubway/json/TransitAlert.java
+++ src/main/java/hubway/json/TransitAlert.java
@@ -1,25 +1,14 @@
 package hubway.json;
 
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.javadocmd.simplelatlng.LatLng;
-
 public class TransitAlert {
 
-	public HashMap <String,LinkedList<String>> linesAndStations;
-	
-	public TransitAlert()
-	{
-		linesAndStations = new HashMap<>();		
+	public HashMap<String, LinkedList<String>> linesAndStations;
+
+	public TransitAlert() {
+		linesAndStations = new HashMap<String, LinkedList<String>>();
 	}
-	
 
 }
